ac6e0f080972bf6f46d3cbe8439ac3745ddbc66f
Bruna Cassola <bruunascassola@gmail.com>
2024-11-23 17:47:55 +0000

    Update ft_printf_functions_nbr.c

--- My library/ft_printf_functions_nbr.c
+++ My library/ft_printf_functions_nbr.c
@@ -12,36 +12,36 @@
 
 #include "ft_printf.h"
 
-int	ft_putnbr(int number)
+int	printf_putnbr(int number)
 {
 	int	i;
 
 	i = 0;
 	if (number == -2147483648)
-		return (ft_putstr("-2147483648"));
+		return (printf_putstr("-2147483648"));
 	if (number < 0)
 	{
-		i += ft_putchar('-');
+		i += printf_putchar('-');
 		number = -number;
 	}
 	if (number >= 10)
-		i += ft_putnbr(number / 10);
-	i += ft_putchar((number % 10) + '0');
+		i += printf_putnbr(number / 10);
+	i += printf_putchar((number % 10) + '0');
 	return (i);
 }
 
-int	ft_putnbr_unsigned(unsigned int number)
+int	printf_putnbr_unsigned(unsigned int number)
 {
 	int	i;
 
 	i = 0;
 	if (number >= 10)
-		i += ft_putnbr_unsigned(number / 10);
-	i += ft_putchar((number % 10) + '0');
+		i += printf_putnbr_unsigned(number / 10);
+	i += printf_putchar((number % 10) + '0');
 	return (i);
 }
 
-int	ft_putnbr_hex(unsigned int number, int lowercase)
+int	printf_putnbr_hex(unsigned int number, int lowercase)
 {
 	int		i;
 	char	*base;
@@ -52,7 +52,7 @@ int	ft_putnbr_hex(unsigned int number, int lowercase)
 	else
 		base = "0123456789ABCDEF";
 	if (number >= 16)
-		i += ft_putnbr_hex(number / 16, lowercase);
-	i += ft_putchar(base[number % 16]);
+		i += printf_putnbr_hex(number / 16, lowercase);
+	i += printf_putchar(base[number % 16]);
 	return (i);
 }
