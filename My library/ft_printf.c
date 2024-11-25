faca9eaeea7652f6c3697a9d2c730cacd6e2eb81
Bruna Cassola <bruunascassola@gmail.com>
2024-11-25 10:49:37 +0000

    Update ft_printf.c

--- My library/ft_printf.c
+++ My library/ft_printf.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "libft.h"
 
 int	put_placeholder(char type, va_list args)
 {
