02f9621cb4ac33067b6ce829b1fef78fb57e219f
Bruna Cassola <bruunascassola@gmail.com>
2024-11-25 10:51:32 +0000

    Update get_next_line_utils.c

--- My library/get_next_line_utils.c
+++ My library/get_next_line_utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "libft.h"
 
 int	gnl_strlen(char *str)
 {
