f947630c8af6bc2e3e6e0a581dd1c29eef6cf826
Bruna Cassola <bruunascassola@gmail.com>
2024-11-25 10:13:13 +0000

    Update client.c

--- Common-Core/Milestone 3/minitalk/client.c
+++ Common-Core/Milestone 3/minitalk/client.c
@@ -1,4 +1,11 @@
-#include <signal.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brsoares <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 09:47:21 by brsoares          #+#    #+#             */#include <signal.h>
 #include "libft/libft.h"
 
 void send_bit(int target_pid, unsigned char ch)
@@ -40,12 +47,63 @@ int main(int argc, char **argv)
         ft_printf("Invalid PID\n");
         return 1;
     }
-    if (argv[2] == NULL || argv[2][0] == '\0')
-    {
-        ft_printf("Error: Message cannot be null or empty\n");
-        return 1;
-    }
-    
-    send_message(target_pid, argv[2]);
-    return 0;
+    if (argv[2] == NULL
+/*   Updated: 2024/11/25 10:04:23 by brsoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+#include "libft/libft.h"
+
+void	send_bit(int target_pid, unsigned char ch)
+{
+	int	bit_position;
+
+	bit_position = 7;
+	while (bit_position >= 0)
+	{
+		if ((ch >> bit_position) & 1)
+			kill(target_pid, SIGUSR2);
+		else
+			kill(target_pid, SIGUSR1);
+		usleep(300);
+		bit_position--;
+	}
+}
+
+void	send_message(int target_pid, const char *str)
+{
+	size_t		i;
+
+	i = 0;
+	while (str[i])
+	{
+		send_bit(target_pid, str[i]);
+		i++;
+	}
+	send_bit(target_pid, '\0');
+}
+
+int	main(int argc, char **argv)
+{
+	int	target_pid;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: ./client <server_pid> <message>\n");
+		return (1);
+	}
+	target_pid = atoi(argv[1]);
+	if (target_pid <= 0)
+	{
+		ft_printf("Invalid PID\n");
+		return (1);
+	}
+	if (argv[2] == NULL || argv[2][0] == '\0')
+	{
+		ft_printf("Error: Message cannot be null or empty\n");
+		return (1);
+	}
+	send_message(target_pid, argv[2]);
+	return (0);
 }
