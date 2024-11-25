a96ee828441780dd441c3b53d7df71b41d9ab7ad
Bruna Cassola <bruunascassola@gmail.com>
2024-11-25 10:50:14 +0000

    Update ft_printf_functions_nbr.c

--- My library/ft_printf_functions_nbr.c
+++ My library/ft_printf_functions_nbr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "libft.h"
 
 int	printf_putnbr(int number)
 {
