047cfb064da4caad94fbb3d7b5dfc3a78a9d2875
Bruna Cassola <bruunascassola@gmail.com>
2024-11-30 13:59:00 +0000

    Add files via upload

---
+++ Common-Core/Milestone 3/push_swap/push_swap_utils.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brsoares <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 13:45:18 by brsoares          #+#    #+#             */
+/*   Updated: 2024/11/30 13:48:21 by brsoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <libft/libft.h>
+
+void	ps_push_to_stack(t_stack *src, t_stack *dest)
+{
+
+}
+
+int	ps_sorted(t_stack *stack)
+{
+
+}
+
+void	ps_print_stack(t_stack *stack)
+{
+
+}
+
+void	ps_error_exit(const char *message)
+{
+
+}
+
+void	ps_sort_stack(t_stack *stack_a, t_stack *stack_b)
+{
+
+}
---
+++ Common-Core/Milestone 3/push_swap/push_swap_utils2.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brsoares <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 13:49:12 by brsoares          #+#    #+#             */
+/*   Updated: 2024/11/30 13:51:16 by brsoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <libft/libft.h>
+
+void	ps_push(t_stack *stack, int value)
+{
+
+}
+
+int	ps_pop(t_stack *stack)
+{
+
+}
+
+void	ps_swap(t_stack *stack)
+{
+
+}
+
+void	ps_rotate(t_stack *stack)
+{
+
+}
+
+void	ps_reverse_rotate(t_stack *stack)
+{
+
+}
