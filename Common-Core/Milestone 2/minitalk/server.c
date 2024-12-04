de95f2a1bf01876202598f9b3b290ea6452077a6
Bruna Cassola <bruunascassola@gmail.com>
2024-12-04 10:36:29 +0000

    Update server.c

--- Common-Core/Milestone 2/minitalk/server.c
+++ Common-Core/Milestone 2/minitalk/server.c
@@ -14,39 +14,44 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
-void	handle_signal(int signal, siginfo_t *info, void *extra)
+void	handle_signal(int signal)
 {
 	static int		bit;
 	static unsigned char	ch;
 
-	(void)extra;
-	if (signal == SIGUSR2)
+	if (!ch)
+		ch = 0;
+	if (!bit)
+		bit = 0;
+	if (signal == SIGUSR1)
 		ch = ch | (1 << (7 - bit));
+	if (signal == SIGUSR2)
+		ch = ch & ~(1 << (7 - bit));
 	bit++;
 	if (bit == 8)
 	{
-		if (ch == '\0')
-			ft_printf("\n");
-		else
-			ft_printf("%c", ch);
+		ft_printf("%c", ch);
 		bit = 0;
 		ch = 0;
 	}
-	kill(info->si_pid, SIGUSR1);
 }
 
 int	main(void)
 {
 	struct sigaction	sig;
+	pid_t			target_pid;
+	
+	target_pid = getpid();
+	if (target_pid == -1)
 	{
-		sig.sa_flags = SA_SIGINFO;
-		sig.sa_sigaction = handle_signal;
-		sigemptyset(&sig.sa_mask);
-		sigaction(SIGUSR1, &sig, NULL);
-		sigaction(SIGUSR2, &sig, NULL);
+		ft_printf("Invalid PID\n");
+		exit(1);
 	}
-	ft_printf("Server PID: %d\n", getpid());
+	ft_printf("Server PID: %d\n", target_pid);
+	sig.sa_flags = 0;
+	sig.sa_handler = &handle_signal;
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
 	while (1)
 		pause();
-	return (0);
 }
