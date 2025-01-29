7c0fc17cb2cccb9692ca870e8107b38ed157f41d
Bruna Cassola <bruunascassola@gmail.com>
2025-01-29 09:31:32 +0000

    Update server.c

--- Common-Core/Milestone 2/minitalk/server.c
+++ Common-Core/Milestone 2/minitalk/server.c
@@ -14,15 +14,36 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
+static void	handle_char(char ch, char **str)
+{
+	char	temp[2];
+	char	*new_str;
+
+	temp[0] = ch;
+	temp[1] = '\0';
+	if (!*str)
+		*str = ft_strdup(temp);
+	else if (ch == 0)
+	{
+		ft_printf("%s\n", *str);
+		free(*str);
+		*str = NULL;
+	}
+	else
+	{
+		new_str = ft_strjoin(*str, temp);
+		free(*str);
+		*str = ft_strdup(new_str);
+		free(new_str);
+	}
+}
+
 void	handle_signal(int signal)
 {
 	static int	bit;
 	static char	ch;
+	static char	*str;
 
-	if (!ch)
-		ch = 0;
-	if (!bit)
-		bit = 0;
 	if (signal == SIGUSR1)
 		ch = ch | (1 << (7 - bit));
 	if (signal == SIGUSR2)
@@ -30,7 +51,7 @@ void	handle_signal(int signal)
 	bit++;
 	if (bit == 8)
 	{
-		ft_printf("%c", ch);
+		handle_char(ch, &str);
 		bit = 0;
 		ch = 0;
 	}
