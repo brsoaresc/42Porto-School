94871bc81f5409064cb3e9502b0f5993385d4208
Bruna Cassola <bruunascassola@gmail.com>
2025-02-11 15:02:39 +0000

    Update repeat_alpha.c

--- 42-Exams/Rank_2/Level_1/repeat_alpha.c
+++ 42-Exams/Rank_2/Level_1/repeat_alpha.c
@@ -35,24 +35,24 @@ $>
 int	main(int argc, char **argv)
 {
 	int	i;
-	int	c;
+	int	repeat;
 
 	i = 0;
 	if (argc == 2)
 	{
 		while(argv[1][i])
 		{
-			c = argv[1][i];
+			repeat = argv[1][i];
 			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-				c = argv[1][i] - 'a' + 1;
+				repeat = argv[1][i] - 'a' + 1;
 			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-				c = argv[1][i] - 'A' + 1;
+				repeat = argv[1][i] - 'A' + 1;
 			else
-				c = 1;
-			while(c > 0)
+				repeat = 1;
+			while(repeat > 0)
 			{
 				write(1, &argv[1][i], 1);
-				c--;
+				repeat--;
 			}
 			i++;
 
