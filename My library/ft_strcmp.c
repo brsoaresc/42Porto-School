b4f223352a8f096144a352139aec422b92d9be03
Bruna Cassola <bruunascassola@gmail.com>
2025-02-12 13:54:02 +0000

    Add files via upload

---
+++ My library/ft_strcmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brsoares <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 18:59:55 by brsoares          #+#    #+#             */
+/*   Updated: 2025/02/11 19:00:03 by brsoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] - s2[i] == 0)
+			i++;
+		else
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+	}
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+}
