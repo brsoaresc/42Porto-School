1cf15b8258da60c3d8824d3c56fe303d7aa7ccaf
Bruna Cassola <bruunascassola@gmail.com>
2025-02-12 13:52:08 +0000

    Update get_next_line.c

--- My library/get_next_line.c
+++ My library/get_next_line.c
@@ -12,6 +12,14 @@
 
 #include "libft.h"
 
+void	gnl_free_data(char *buffer, char *str)
+{
+	if (buffer)
+		free(buffer);
+	if (str)
+		free(str);
+}
+
 char	*gnl_read_file(int fd, char *new_str)
 {
 	char	*buffer;
@@ -19,75 +27,53 @@ char	*gnl_read_file(int fd, char *new_str)
 
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
-		return (NULL);
+		return (gnl_free_data(NULL, new_str), NULL);
 	read_bytes = 1;
 	while (!gnl_strchr(new_str, '\n') && read_bytes > 0)
 	{
 		read_bytes = read(fd, buffer, BUFFER_SIZE);
 		if (read_bytes == -1)
-			return (free(buffer), free(new_str), NULL);
+			return (gnl_free_data(buffer, new_str), NULL);
 		buffer[read_bytes] = '\0';
 		new_str = gnl_strjoin(new_str, buffer);
 		if (!new_str)
-		{
-			free(buffer);
-			return (NULL);
-		}
+			return (gnl_free_data(buffer, NULL), NULL);
 	}
-	free(buffer);
+	gnl_free_data(buffer, NULL);
 	return (new_str);
 }
 
 char	*gnl_extract_line(char *str)
 {
-	int		i;
-	char	*line;
+	int	i;
+	int	len;
 
-	i = 0;
 	if (!str || !str[0])
 		return (NULL);
-	while (str[i] && str[i] != '\n')
-		i++;
-	line = malloc((i + 2) * sizeof(char));
-	if (!line)
-		return (NULL);
 	i = 0;
 	while (str[i] && str[i] != '\n')
-	{
-		line[i] = str[i];
 		i++;
-	}
 	if (str[i] == '\n')
-		line[i++] = '\n';
-	line[i] = '\0';
-	return (line);
+		len = i + 1;
+	else
+		len = i;
+	return (gnl_strndup(str, len));
 }
 
 char	*gnl_reset_str(char *str)
 {
 	int		i;
-	int		j;
 	char	*new_str;
 
-	i = 0;
 	if (!str)
 		return (NULL);
+	i = 0;
 	while (str[i] && str[i] != '\n')
 		i++;
 	if (!str[i])
-	{
-		free(str);
-		return (NULL);
-	}
-	new_str = (char *) malloc((gnl_strlen(str) - i + 1) * sizeof(char));
-	if (!new_str)
-		return (NULL);
-	i++;
-	j = 0;
-	while (str[i])
-		new_str[j++] = str[i++];
-	new_str[j] = '\0';
-	free(str);
+		return (gnl_free_data(NULL, str), NULL);
+	new_str = gnl_strndup(str + i + 1, gnl_strlen(str) - i - 1);
+	gnl_free_data(NULL, str);
 	return (new_str);
 }
 
@@ -102,6 +88,8 @@ char	*get_next_line(int fd)
 	if (!left_line)
 		return (NULL);
 	line = gnl_extract_line(left_line);
+	if (!line)
+		return (gnl_free_data(NULL, left_line), NULL);
 	left_line = gnl_reset_str(left_line);
 	return (line);
 }
